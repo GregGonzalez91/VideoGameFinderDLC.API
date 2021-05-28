9f8dbaed25c7dae2fb53edf4c482b931e3cb19ff
Greg Gonzalez <gregery.gonzalez@gmail.com>
2021-05-27 20:40:58 -0400

    Fixed Game Controller

--- VideoGameFinderDLC.API/Controllers/GameController.cs
+++ VideoGameFinderDLC.API/Controllers/GameController.cs
@@ -2,7 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using VideoGameFinderDLC.Models;
 using VideoGameFinderDLC.Services;
