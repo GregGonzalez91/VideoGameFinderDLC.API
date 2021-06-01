307ee7f082d48a6cb30a762f167cc3bd03bb0d3a
ksgraves <kraegan.s.g@gmail.com>
2021-06-01 18:05:48 -0400

    2nd push after CRUD build

--- VideoGameFinderDLC.API/App_Start/IdentityConfig.cs
+++ VideoGameFinderDLC.API/App_Start/IdentityConfig.cs
@@ -21,7 +21,7 @@ namespace VideoGameFinderDLC.API
         {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
             // Configure validation logic for usernames
-            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager) 
             {
                 AllowOnlyAlphanumericUserNames = false,
                 RequireUniqueEmail = true
