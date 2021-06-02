720a35adf22f51599f48a0eead662d8c77b2e290
ksgraves <kraegan.s.g@gmail.com>
2021-06-01 20:01:02 -0400

    Test Commit

--- VideoGameFinderDLC.Data/IdentityModels.cs
+++ VideoGameFinderDLC.Data/IdentityModels.cs
@@ -7,10 +7,10 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
-namespace VideoGameFinderDLC.Data 
+namespace VideoGameFinderDLC.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser 
+    public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
@@ -39,7 +39,7 @@ namespace VideoGameFinderDLC.Data
 
         //public DbSet<UserRating> UserRatings { get; set; }
 
-        //public DbSet<GameGenre> GameGenres { get; set; }
+       // public DbSet<GameGenre> GameGenres { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
