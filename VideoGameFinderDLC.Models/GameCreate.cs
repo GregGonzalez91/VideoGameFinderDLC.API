216d029d55fe965e210afcb6c8433870b2b4dea2
ksgraves <kraegan.s.g@gmail.com>
2021-06-08 17:51:14 -0400

    Initial Push

--- VideoGameFinderDLC.Models/GameCreate.cs
+++ VideoGameFinderDLC.Models/GameCreate.cs
@@ -10,10 +10,10 @@ namespace VideoGameFinderDLC.Models
     {
         public string GameTitle { get; set; }
         public double GamePrice { get; set; }
-        //public string GameSystemName { get; set; }
+        //public string GameSystemName { get; set; }  **This information is pulled with GameSystemId
         public string ESRBRating { get; set; }
         public bool IsReccommended { get; set; }
-        //public string GenreType { get; set; }
+        //public string GenreType { get; set; } **This information is pulled with GameGenreId
         public int ReleaseDate { get; set; }
         public int PlayerCount { get; set; }
         public int OwnerId { get; set; }
